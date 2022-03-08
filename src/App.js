f18ff90c82c17c5eb930b55dfc5ad2246aea5aea
mk1encer <mksur1er@gmail.com>
2022-03-08 23:11:58 +0900

    modify small things

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import react, { useState } from "react";
 import AddUser from "./Components/Users/AddUser";
 import UsersList from "./Components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
   return (
     <div>
       <AddUser />
